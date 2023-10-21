0d3661705e6e71ac168e548fb6e105898549129d
umairgakher <umair.gakher5@gmail.com>
2023-10-21 23:55:00 +0500

    comments

--- lib/Admin/drivers.dart
+++ lib/Admin/drivers.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors, non_constant_identifier_names, unnecessary_cast, prefer_const_constructors, avoid_unnecessary_containers, sort_child_properties_last
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
