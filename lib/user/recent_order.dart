f0c58cd43be522a01b1d2d9d6707d30939309d80
umairgakher <umair.gakher5@gmail.com>
2023-12-15 14:48:01 +0500

    sda

--- lib/user/recent_order.dart
+++ lib/user/recent_order.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: non_constant_identifier_names, prefer_const_constructors, sort_child_properties_last, unused_element
+// ignore_for_file: non_constant_identifier_names, prefer_const_constructors, sort_child_properties_last, unused_element, avoid_print
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
